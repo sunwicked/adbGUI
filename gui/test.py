9dcd7696aa8add7c0405c2f8c27cfdc494bb37ef
sunny <sunnykrditya@gmail.com>
2019-03-04 19:33:46 +0530

    - Finding Package name - Setting value globally

--- gui/test.py
+++ gui/test.py
@@ -3,27 +3,33 @@ import os
 
 from tkinter import filedialog
 
+PACKAGE_FILE_PATH = "/Users/ken/Documents/name.txt"
+
 top = Tkinter.Tk()
 
 
+package = "test"
 
 
 
 def disconnect_call_back():
     os.system("adb disconnect")
 
-
-def uninstall_call_back():
-    os.system("adb uninstall" + "")  # get package name
-
-
 def reboot_call_back():
     os.system("adb reboot")
 
 def pick_apk_callback():
     filename = filedialog.askopenfilename(initialdir="/", title="Select file")
+    os.system("aapt dump badging " + filename + " | awk 'NR==1{print $2}' > " + PACKAGE_FILE_PATH)
+    f = open(PACKAGE_FILE_PATH, 'r')
+    global package
+    package = f.read().split("'")[1]
     os.system("adb install " + filename)
 
+
+def uninstall_call_back():
+    print package
+    os.system("adb uninstall" + package)  # get package name
 
 Pick = Tkinter.Button(top, bg="#000000", text="Pick file", command=pick_apk_callback)
 Disconnect = Tkinter.Button(top, bg="#000000", text="ADB disconnect", command=disconnect_call_back)
