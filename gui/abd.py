eaa4b48e7fdee51fcab2d579d33eccd280978939
sunny <sunnykrditya@gmail.com>
2019-03-24 02:10:05 +0530

    - [WIP] File picker

--- gui/abd.py
+++ gui/abd.py
@@ -4,9 +4,7 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
-from tkinter import filedialog
-import matplotlib
-matplotlib.use('TkAgg')
+from kivy.uix.filechooser import FileChooserListView
 
 
 package = "com.test"
@@ -26,6 +24,9 @@ def reboot_call_back(args):
 def connect_call_back(args):
     os.system("adb connect " + ip)
 
+def show_file_picker_callback(args):
+    return MyWidget()
+
 
 def set_ip_call_back(instance, value):
     global ip
@@ -38,8 +39,7 @@ def uninstall_call_back(args):
     os.system("adb uninstall" + package)  # get package name
 
 
-def pick_apk_install_callback(args):
-    file_path = ""# file picker needs to return the path  #filedialog.askopenfilename(initialdir="/", title="Select file")
+def pick_apk_install_callback(file_path):
     os.system("aapt dump badging " + file_path + " | awk 'NR==1{print $2}' > " + PACKAGE_FILE_PATH)
     f = open(PACKAGE_FILE_PATH, 'r')
     global package
@@ -48,6 +48,33 @@ def pick_apk_install_callback(args):
         os.system("adb install " + file_path)
     else:
         print("failed")
+
+
+class MyWidget(BoxLayout):
+
+    def __init__(self, **kwargs):
+        super(MyWidget, self).__init__(**kwargs)
+
+        container = BoxLayout(orientation='vertical')
+
+        filechooser = FileChooserListView()
+        filechooser.bind(on_selection=lambda x: self.selected(filechooser.selection))
+
+        open_btn = Button(text='open', size_hint=(1, .2))
+        open_btn.bind(on_release=lambda x: self.open(filechooser.path, filechooser.selection))
+
+        container.add_widget(filechooser)
+        container.add_widget(open_btn)
+        self.add_widget(container)
+
+    def open(self, path, filename):
+        print(filename)
+        if len(filename) > 0:
+            with open(os.path.join(path, filename[0])) as f:
+                print(f.read())
+
+    def selected(self, filename):
+        pick_apk_install_callback(filename)
 
 
 class KivyButton(App):
@@ -83,14 +110,14 @@ class KivyButton(App):
         self.btn_install = Button(
             text="ADB install",
             size_hint=(.3, .1),
-            on_press=pick_apk_install_callback
+            on_press=show_file_picker_callback
         )
         textinput = TextInput(hint_text='Input IP',
                               size_hint=(.5, .1),
                               multiline=False
                               )
         textinput.bind(text=set_ip_call_back)
-
+        self.box_ip.add_widget(textinput)
         self.box_ip.add_widget(self.btn_connect)
         self.box.add_widget(self.box_ip)
         self.box.add_widget(self.btn_disconnect)
