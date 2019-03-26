ab818149d9ac6bda36effb2c6366f7da458afd96
sunny <sunnykrditya@gmail.com>
2019-03-27 00:34:26 +0530

    - Using subprocess to execute command line - Adding logs

--- gui/test.py
+++ gui/test.py
@@ -1,33 +1,37 @@
 import subprocess
 import os
-import sys
 import tkinter
 from tkinter import filedialog
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 LOGCAT_FILE_PATH = dir_path + "/log.txt"
+ip = "0.0.0.0:5555"
+package = "test"
+log = ""
 
 top = tkinter.Tk()
 
-package = "test"
-
 
 def disconnect_call_back():
-    os.system("adb disconnect")
+    console_log = subprocess.run(['adb', 'disconnect'], stdout=subprocess.PIPE)
+    convert_to_str(console_log)
 
 
 def connect_call_back():
-    ip = Entry.get()
-    os.system("adb connect " + ip)
+    global ip
+    ip = entry.get()
+    ip_value['text'] = ip
+    console_log = subprocess.run(['adb', 'connect', 'ip'], stdout=subprocess.PIPE)
+    convert_to_str(console_log)
 
 
 def reboot_call_back():
-    os.system("adb reboot")
+    console_log = subprocess.run(['adb', 'reboot'], stdout=subprocess.PIPE)
+    convert_to_str(console_log)
 
 
 def pick_apk_callback():
     filename = filedialog.askopenfilename(initialdir="/", title="Select file")
-    # cmd  = "aapt dump badging " + filename + " | awk 'NR==1{print $2}'"
     global package
     package = subprocess.run(
         ['aapt', 'dump', 'badging', filename],
@@ -37,6 +41,7 @@ def pick_apk_callback():
     if std_out:
         package = std_out.split("versionCode")[0]
         package = package.split("'")[1]
+        package_value['text'] = package
         subprocess.run(
             ['adb', 'install', filename],
             stdout=subprocess.PIPE)
@@ -45,7 +50,8 @@ def pick_apk_callback():
 
 
 def uninstall_call_back():
-    os.system("adb uninstall" + package)  # get package name
+    console_log = subprocess.run(['adb', 'uninstall', package], stdout=subprocess.PIPE)
+    convert_to_str(console_log)
 
 
 def logcat_to_file_callback():
@@ -53,30 +59,46 @@ def logcat_to_file_callback():
 
 
 def clear_call_back():
-    os.system("adb shell pm clear " + package)  # get package name
+    console_log = subprocess.run(['adb', 'shell', 'pm', 'clear', package], stdout=subprocess.PIPE)
+    convert_to_str(console_log)
 
 
-Label = tkinter.Label(top, text="Enter IP")
-Label.grid(row=0, column=0)
+def convert_to_str(input_val):
+    log_value['text'] = str(input_val.stdout)
 
-Entry = tkinter.Entry(top)
-Entry.grid(row=0, column=1)
 
-Connect = tkinter.Button(top, text="ADB connect", command=connect_call_back)
-Connect.grid(row=0, column=2)
-Pick = tkinter.Button(top, bg="#000000", text="ADB install", command=pick_apk_callback)
-Pick.grid(row=2, column=0)
-IpLabel = tkinter.Label(top, text=" Package Name")
-IpLabel.grid(row=2, column=1)
-Disconnect = tkinter.Button(top, bg="#000000", text="ADB disconnect", command=disconnect_call_back)
-Disconnect.grid(row=3, column=0)
-Uninstall = tkinter.Button(top, bg="#000000", text="ADB uninstall", command=uninstall_call_back)
-Uninstall.grid(row=4, column=0)
-Reboot = tkinter.Button(top, bg="#000000", text="ADB reboot", command=reboot_call_back)
-Reboot.grid(row=5, column=0)
-Clear = tkinter.Button(top, bg="#000000", text="ADB clear", command=clear_call_back)
-Clear.grid(row=6, column=0)
-Logcat = tkinter.Button(top, bg="#000000", text="ADB logcat", command=logcat_to_file_callback)
-Logcat.grid(row=7, column=0)
+label = tkinter.Label(top, text=" Enter IP ")
+label.grid(row=0, column=0)
+
+entry = tkinter.Entry(top)
+entry.grid(row=0, column=1)
+
+connect = tkinter.Button(top, text="ADB connect", command=connect_call_back)
+connect.grid(row=0, column=2)
+pick = tkinter.Button(top, bg="#000000", text=" ADB install ", command=pick_apk_callback)
+pick.grid(row=2, column=0)
+
+disconnect = tkinter.Button(top, bg="#000000", text=" ADB disconnect ", command=disconnect_call_back)
+disconnect.grid(row=3, column=0)
+uninstall = tkinter.Button(top, bg="#000000", text=" ADB uninstall ", command=uninstall_call_back)
+uninstall.grid(row=4, column=0)
+reboot = tkinter.Button(top, bg="#000000", text=" ADB reboot ", command=reboot_call_back)
+reboot.grid(row=5, column=0)
+clear = tkinter.Button(top, bg="#000000", text=" ADB clear ", command=clear_call_back)
+clear.grid(row=6, column=0)
+# Logcat = tkinter.Button(top, bg="#000000", text="ADB logcat", command=logcat_to_file_callback)
+# Logcat.grid(row=7, column=0)
+ip_label = tkinter.Label(top, text=" IP::")
+ip_label.grid(row=8, column=0)
+ip_value = tkinter.Label(top, text=ip)
+ip_value.grid(row=8, column=1)
+package_label = tkinter.Label(top, text=" Package Name::")
+package_label.grid(row=9, column=0)
+package_value = tkinter.Label(top, text=package)
+package_value.grid(row=9, column=1)
+log_label = tkinter.Label(top, text=" Log::")
+log_label.grid(row=10, column=0)
+log_value = tkinter.Label(top, text=log)
+log_value.grid(row=10, column=1)
 
 top.mainloop()
